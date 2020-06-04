a1b73279c3269ff0ffe966a44a9715d5f7db9fdb
Maciej Runo <maciej.runo@gmail.com>
2020-06-04 21:09:00 +0200

    Create views and controllers

---
+++ src/Controller/ForecastController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ForecastController extends AbstractController
+{
+    /**
+     * @Route("/forecast", name="forecast")
+     */
+    public function index()
+    {
+        return $this->render('forecast/index.html.twig', [
+            'temp' => 26,
+            'wind' => 16,
+            'humidity' => 16,
+            'wind' => 16,
+        ]);
+    }
+}
