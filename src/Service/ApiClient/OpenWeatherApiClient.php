4a3fbb354927d463506c33d1c4d8d5e6aef7c380
Maciej Runo <maciej.runo@gmail.com>
2020-06-07 13:05:41 +0200

    Add default http client

--- src/Service/ApiClient/OpenWeatherApiClient.php
+++ src/Service/ApiClient/OpenWeatherApiClient.php
@@ -4,16 +4,19 @@ declare(strict_types=1);
 namespace App\Service\ApiClient;
 
 use App\Entity\ApiData;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class OpenWeatherApiClient implements WeatherApiClientInterface
 {
     private $baseUrl;
     private $apiKey;
+    private $client;
 
-    public function __construct(string $baseUrl, string $apiKey)
+    public function __construct(string $baseUrl, string $apiKey, HttpClientInterface $client)
     {
         $this->baseUrl = $baseUrl;
         $this->apiKey = $apiKey;
+        $this->client = $client;
     }
 
     public function getApiData(string $city, string $country): ApiData
