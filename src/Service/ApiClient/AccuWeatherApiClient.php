95a657f6746043541e8afc1e73bb0f4e61a01811
Maciej Runo <maciej.runo@gmail.com>
2020-06-07 13:06:38 +0200

    Give up Yahoo Api for Accu Weather Api

---
+++ src/Service/ApiClient/AccuWeatherApiClient.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Service\ApiClient;
+
+use App\Entity\ApiData;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class AccuWeatherApiClient implements WeatherApiClientInterface
+{
+    private $baseUrl;
+    private $apiKey;
+    private $client;
+
+    public function __construct
+    (
+        string $baseUrl, 
+        string $apiKey, 
+        HttpClientInterface $client
+    ) {
+        $this->baseUrl = $baseUrl;
+        $this->apiKey = $apiKey;
+        $this->client = $client;
+    }
+
+    public function getApiData(string $city, string $country): ApiData
+    {
+    }
+}
