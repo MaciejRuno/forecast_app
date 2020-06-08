9210a7801486342a585fe5d565831502dfeb4cba
Maciej Runo <maciej.runo@gmail.com>
2020-06-08 08:13:15 +0200

    Add averaging logic

--- src/Service/ForecastService.php
+++ src/Service/ForecastService.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace App\Service;
 
+use App\Entity\RequestData;
 use App\Service\ApiClient\WeatherApiClientInterface;
 
 class ForecastService 
@@ -14,8 +15,53 @@ class ForecastService
         $this->weatherApis[] = $api;
     }
 
-    public function getApis()
+    public function getRequestData
+    (
+        string $city, 
+        string $country, 
+        string $clientIp
+    ): ?RequestData {
+        $apiData = [];
+
+        $requestData = new RequestData();
+        $requestData->setUserIp(\ip2long($clientIp));
+        $requestData->setCreatedAt(new \DateTime());
+
+
+        foreach ($this->weatherApis as $api) {
+            $apiDatum = $api->getApiData($city, $country);
+
+            if ($apiDatum) {
+                $requestData->addApiData($apiDatum);
+                $apiData[] = $apiDatum;
+            }
+        }
+
+        return $requestData;
+    }
+
+    public function getForecastFromRequestData(RequestData $requestData): array
     {
-        return $this->weatherApis;
+        $allForecasts = [
+            'temperature' => [],
+            'wind' => [],
+            'humidity' => [],
+            'rainfall' => [],
+        ];
+
+        foreach ($requestData->getApiData() as $apiData) {
+            $allForecasts['temperature'][] = $apiData->getTemperature();
+            $allForecasts['wind'][] = $apiData->getWind();
+            $allForecasts['humidity'][] = $apiData->getHumidity();
+            $allForecasts['rainfall'][] = $apiData->getRainfall();
+        }
+
+        $averageForecast = [];
+
+        foreach ($allForecasts as $key => $values) {
+            $averageForecast[$key] = \array_sum($values) / \count($values);
+        }
+
+        return $averageForecast;
     }
 }
