87be3d58de1d60d20c901a32afdbfb16d009ca58
Maciej Runo <maciej.runo@gmail.com>
2020-06-09 21:04:42 +0200

    Add entity saving

--- src/Service/ForecastService.php
+++ src/Service/ForecastService.php
@@ -6,10 +6,19 @@ namespace App\Service;
 use App\Entity\ApiData;
 use App\Entity\RequestData;
 use App\Service\ApiClient\WeatherApiClientInterface;
+use Doctrine\ORM\EntityManagerInterface;
 
 class ForecastService 
 {
+    private $em;
+    private $requestRepository;
+
     private $weatherApis = [];
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
 
     /**
      * @param WeatherApiClientInterface $api
@@ -31,21 +40,21 @@ class ForecastService
         string $country, 
         string $clientIp
     ): ?RequestData {
-        $apiData = [];
-
         $requestData = new RequestData();
         $requestData->setUserIp(\ip2long($clientIp));
         $requestData->setCreatedAt(new \DateTime());
 
-
         foreach ($this->weatherApis as $api) {
-            $apiDatum = $api->getApiData($city, $country);
+            /** @var ApiData $apiData */
+            $apiData = $api->getApiData($city, $country);
 
-            if ($apiDatum) {
-                $requestData->addApiData($apiDatum);
-                $apiData[] = $apiDatum;
+            if ($apiData) {
+                $requestData->addApiData($apiData);
+                $this->em->persist($apiData);
             }
         }
+        $this->em->flush();
+        $this->em->clear();
 
         return $requestData;
     }
