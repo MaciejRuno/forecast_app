63884ad3b866ab77dba9362a7e564fb60900a289
Maciej Runo <maciej.runo@gmail.com>
2020-06-09 21:04:08 +0200

    Add basic logging

--- src/Controller/ForecastController.php
+++ src/Controller/ForecastController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Service\ForecastService;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,17 +17,27 @@ class ForecastController extends AbstractController
      * @param ForecastService $forecastService
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index(Request $request, ForecastService $forecastService)
+    public function index
+    (
+        Request $request, 
+        ForecastService $forecastService,
+        LoggerInterface $logger
+    )
     {
         $city = $request->get('city');
         $country = $request->get('country');
         $clientIp = $request->getClientIp();
+
+        $logger->info("getting data for $city, $country");
 
         $requestData = $forecastService->getRequestData(
             $city, 
             $country, 
             $clientIp
         );
+
+        $apisResponded = \count($requestData->getApiData());
+        $logger->info("got responses from $apisResponded apis");
 
         $forecast = $forecastService->getForecastFromRequestData($requestData);
 
